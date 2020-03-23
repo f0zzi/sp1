e6a741a46a373036527c616920d9fc4c013ab324
Alexander Goncharuk <sashael@ukr.net>
2020-03-23 14:37:05 +0200

    ready

--- sp1/MainWindow.xaml.cs
+++ sp1/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
 
 namespace sp1
 {
@@ -20,9 +22,47 @@ namespace sp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ListView listView = new ListView();
+
         public MainWindow()
         {
             InitializeComponent();
+            this.Width = 250;
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(9, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            Button button = new Button { Content = "Get threads" };
+            grid.Children.Add(button);
+            grid.Children.Add(listView);
+
+            GridView gridView = new GridView();
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "Name",
+                Width = 160,
+                DisplayMemberBinding = new Binding("Name")
+            });
+            gridView.Columns.Add(new GridViewColumn
+            {
+                Header = "Id",
+                Width = 50,
+                DisplayMemberBinding = new Binding("Id"),
+            });
+            listView.View = gridView;
+
+            Grid.SetRow(listView, 0);
+            Grid.SetRow(button, 1);
+
+            button.Click += Get_threads_Click;
+        }
+
+        private void Get_threads_Click(object sender, RoutedEventArgs e)
+        {
+            listView.Items.Clear();
+            Process[] p = Process.GetProcesses().OrderBy(x => x.Id).ToArray();
+            foreach (Process item in p)
+            {
+                listView.Items.Add(new { Id = item.Id, Name = item.ProcessName });
+            }
         }
     }
 }
